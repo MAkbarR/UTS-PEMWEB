a034228c930bfbfa4b35e9e540aedebd59aed14c
MAkbarR <90317427+MAkbarR@users.noreply.github.com>
2021-11-03 17:08:17 +0700

    Add files via upload

---
+++ websik/jq.js
@@ -0,0 +1,9 @@
+function remove1() {
+    var elem = document.getElementById('c1');
+    elem.parentNode.removeChild(elem);
+   }
+
+   function remove2() {
+    var elem = document.getElementById('c2');
+    elem.parentNode.removeChild(elem);
+   }
